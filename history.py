2b521ca112cb09e2b871ac897484123f13193e17
asemqaffaf <asem@qaffaf.com>
2019-11-19 15:48:46 +0200

    python

---
+++ history.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Nov 18 15:52:36 2019)---
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
+1+1
+a =10
+b =20
+a+b
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
+
+## ---(Tue Nov 19 11:17:17 2019)---
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
+5 /2
+5//2
+15.2
+153.3//
+153.3//0
+153.3/0
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
+
+print?
+
+## ---(Tue Nov 19 14:45:18 2019)---
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
+3
+runfile('/home/owner/.config/spyder-py3/temp.py', wdir='/home/owner/.config/spyder-py3')
---
+++ temp.py
@@ -0,0 +1,66 @@
+
+print('{:10} {:15} {:20}'.format("Hello\n", "Hello\n" , "Hello\n"))
+
+print("Orange Acadmey\n" *20)
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float(4.2)
+print(x,y,z,w,sep='10')
+
+num1 ,num2 = 10, 20
+print(num1 * num2)
+
+#n1, n2, n3, n4, n5 =  input("enter num") , input("enter num")  , input("enter num") ,  input("enter num") ,  input("enter num")
+#print(int(n1)+int(n2)+int(n3)+int(n4)+int(n5)/5)
+
+x = 1
+y = 2.8
+z = 1j
+o = "Orange"
+A = True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+
+x, y = "Orange" , 1
+x1,y1 = 100,10
+print(x)
+print(y)
+print(x1)
+print(y1)
+
+x5 = 10
+print(x5)
+x_q = 10
+A = 14
+print(A)
+print(10*10)
+
+print("cheers") #"this is program
+
+#name,age = input("enter your name :"), input("enter your age :")
+#print( name, 2019 - int(age) + 100)
+
+#base , height = input("base") , input("height")
+#print(.5 * int(base) + int(height))
+
+x = 11
+y = 3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x+y)
+print(x*-1)
+print(int(x))
+print(float(x))
+print(divmod(x))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
